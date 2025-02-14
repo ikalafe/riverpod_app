f259aa1d5717dcb17613c47948e234ff8c0300e2
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-14 18:07:59 +0330

    feat(products): add Riverpod providers for product management
    - Implemented `productsProvider` to manage a list of all products.
    - Added `reduceProductsProvider` to filter products priced below 50.
    - Generated corresponding Riverpod provider code for state management.
    - Ensures modularity and scalability for product handling in the app.
    
    This commit introduces structured product state management using Riverpod.

---
+++ lib/providers/products_provider.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'products_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productsHash() => r'71142ae3ed0349ae544109b0490fec9e77bcaceb';
+
+/// See also [products].
+@ProviderFor(products)
+final productsProvider = AutoDisposeProvider<List<Product>>.internal(
+  products,
+  name: r'productsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$productsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ProductsRef = AutoDisposeProviderRef<List<Product>>;
+String _$reduceProductsHash() => r'3db20c607b911a8bcc8c5c9f285c4399781bb277';
+
+/// See also [reduceProducts].
+@ProviderFor(reduceProducts)
+final reduceProductsProvider = AutoDisposeProvider<List<Product>>.internal(
+  reduceProducts,
+  name: r'reduceProductsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$reduceProductsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ReduceProductsRef = AutoDisposeProviderRef<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
